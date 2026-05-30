ef781b4eccce04a9f43d4e992adeb9ee329a783a
Nevena <yinsanjin1@gmail.com>
2026-05-30 12:35:14 +0800

    Add idle redirect to homepage

---
+++ js/idle-home-redirect.js
@@ -0,0 +1,62 @@
+(function () {
+  const IDLE_TIMEOUT_MS = 30 * 1000;
+  const ACTIVITY_MESSAGE_TYPE = "bee-project-user-activity";
+  const events = [
+    "mousemove",
+    "mousedown",
+    "keydown",
+    "scroll",
+    "touchstart",
+    "pointerdown",
+    "wheel"
+  ];
+
+  const script = document.currentScript;
+  const homeUrl = script?.dataset.homeUrl || "./index.html";
+  const isEmbedded = window.self !== window.top;
+  let idleTimer = 0;
+
+  function notifyParent() {
+    if (!isEmbedded) {
+      return;
+    }
+    window.parent.postMessage({ type: ACTIVITY_MESSAGE_TYPE }, "*");
+  }
+
+  function goHome() {
+    const currentUrl = new URL(window.location.href);
+    const targetUrl = new URL(homeUrl, window.location.href);
+
+    if (currentUrl.href === targetUrl.href) {
+      resetIdleTimer();
+      return;
+    }
+
+    window.location.href = targetUrl.href;
+  }
+
+  function resetIdleTimer() {
+    window.clearTimeout(idleTimer);
+
+    if (!isEmbedded) {
+      idleTimer = window.setTimeout(goHome, IDLE_TIMEOUT_MS);
+    }
+  }
+
+  function handleActivity() {
+    resetIdleTimer();
+    notifyParent();
+  }
+
+  events.forEach((eventName) => {
+    window.addEventListener(eventName, handleActivity, { passive: true });
+  });
+
+  window.addEventListener("message", (event) => {
+    if (event.data?.type === ACTIVITY_MESSAGE_TYPE) {
+      resetIdleTimer();
+    }
+  });
+
+  resetIdleTimer();
+})();
